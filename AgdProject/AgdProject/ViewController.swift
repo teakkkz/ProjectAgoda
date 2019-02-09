13b2a6f05f5e51ac16a1c72592a6c76f78bb7ef5
teakkkz <47073658+teakkkz@users.noreply.github.com>
2019-02-09 16:58:30 +0700

    Add files via upload

---
+++ AgdProject/AgdProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AgdProject
+//
+//  Created by Suwatchai on 2/2/2562 BE.
+//  Copyright © 2562 suwatchai kiatchalermporn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
